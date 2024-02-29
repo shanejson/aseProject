970b5bad98e36fa445732e772973f8e3b2382341
Sheldon Kevin <sheldonkevin@sheldons-air.home>
2024-02-29 17:20:29 -0500

    added dropdown to select project and added space below to display bar graph

--- src/app/govt-dashboard/govt-dashboard.component.ts
+++ src/app/govt-dashboard/govt-dashboard.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common'; // Import CommonModule
+
+
+
+interface Project {
+  id: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-govt-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './govt-dashboard.component.html',
-  styleUrl: './govt-dashboard.component.css'
+  styleUrls: ['./govt-dashboard.component.css'] // Corrected 'styleUrls'
 })
-export class GovtDashboardComponent {
+export class GovtDashboardComponent implements OnInit {
+  dropdownOptions: Project[] = [];
 
+  selectedProjectId: String = "";
+
+  ngOnInit() {
+    // Retrieve data from local storage
+    const projectsString = localStorage.getItem('projects');
+
+    if (projectsString !== null) {
+      // Parse the data if it's not null
+      const projects: Project[] = JSON.parse(projectsString);
+      
+      // Populate dropdownOptions array with id and name
+      this.dropdownOptions = projects.map((project: Project) => ({ id: project.id, name: project.name }));
+    }
+  }
+
+  showGraph() {
+    console.log('Selected project ID:', this.selectedProjectId);
+  }
 }
